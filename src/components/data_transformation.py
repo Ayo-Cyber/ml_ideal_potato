f2206a91ca990f605b77c5c20e3d716c621dda31
Ayo-Cyber <atunraseayomide@gmail.com>
2023-10-21 23:21:55 +0100

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -72,17 +72,32 @@ class DataTransformation:
            target_column_name = 'math_score'
            numerical_columns = ['writing_score', 'reading_score']
 
-           input_feature_train = train_df.drop(columns=[target_column_name], axis=1)
+           input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
            target_feature_train_df = train_df[target_column_name]
 
            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
            target_feature_test_df = test_df[target_column_name]
 
-           
+           logging.info("Applying Data Transformation On Data")
+
+           input_train_feature_parameter = preprocessing_obj.fit_transform(input_feature_train_df)
+           input_test_feature_parameter = preprocessing_obj.tranform(input_feature_test_df)
+
+           train_arr = np.c_[input_train_feature_parameter, np.array(target_feature_train_df)]
+           test_arr = np.c_[input_test_feature_parameter, np.array(target_feature_test_df)]
+
+           logging.info("Completed Data Transformation")
+
+           return(
+               train_arr,
+               test_arr,
+               self.data_transformation.preprocess_obj_file
+           )
+
+
+
         except:
             pass
 
-
-if __name__ == "__main__":
 
 
